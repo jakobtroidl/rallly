175bf28716ac1a8e972fa442fa721ed164443a7e
Jakob Troidl <jakob.troidl@googlemail.com>
2022-09-22 11:40:04 -0400

    add comments to demo

--- src/components/home/poll-demo.tsx
+++ src/components/home/poll-demo.tsx
@@ -7,11 +7,12 @@ import { ScoreSummary } from "../poll/score-summary";
 
 const sidebarWidth = 180;
 const columnWidth = 100;
+// mock data of poll participants
 const participants = [
   {
-    name: "Reed",
-    color: "bg-sky-400",
-    votes: [0, 2],
+    name: "Reed", // participant name
+    color: "bg-sky-400", // name icon color
+    votes: [0, 2], // idx of times they are available
   },
   {
     name: "Susan",
@@ -30,12 +31,14 @@ const participants = [
   },
 ];
 
+// data options to choose from 
 const options = ["2022-12-14", "2022-12-15", "2022-12-16", "2022-12-17"];
 
 const PollDemo: React.VoidFunctionComponent = () => {
   const { t } = useTranslation("homepage");
 
   const { dayjs } = useDayjs();
+  // build the Dom elements. 
   return (
     <div
       className="rounded-lg bg-white py-1 shadow-huge"
@@ -47,15 +50,15 @@ const PollDemo: React.VoidFunctionComponent = () => {
           style={{ width: sidebarWidth }}
         >
           <div className="flex h-full grow items-end">
-            {t("participantCount", { count: participants.length })}
+            {t("participantCount", { count: participants.length /* display how many people participated in total in the poll*/})}
           </div>
         </div>
-        {options.map((option, i) => {
+        {options.map((option, i) => { // draw the dates that are chooseable in the top row. 
           const d = new Date(option);
           let score = 0;
-          participants.forEach((participant) => {
+          participants.forEach((participant) => { 
             if (participant.votes.includes(i)) {
-              score++;
+              score++; // compute how many participant voted for each time. 
             }
           });
           return (
@@ -67,31 +70,32 @@ const PollDemo: React.VoidFunctionComponent = () => {
               <div>
                 <div className="font-semibold leading-9">
                   <div className="text-sm uppercase text-slate-400">
-                    {dayjs(d).format("ddd")}
+                    {dayjs(d).format("ddd") /* plot the day */}
                   </div>
                   <div className="text-2xl">{dayjs(d).format("DD")}</div>
                   <div className="text-xs font-medium uppercase text-slate-400/75">
-                    {dayjs(d).format("MMM")}
+                    {dayjs(d).format("MMM") /* plot the month*/ }
                   </div>
                 </div>
               </div>
               <div>
-                <ScoreSummary yesScore={score} />
+                {/* plot how many people voted for each time */}
+                <ScoreSummary yesScore={score} /> 
               </div>
             </div>
           );
         })}
       </div>
-      {participants.map((participant, i) => (
+      {participants.map((participant, i) => (  // add one row for each participant to the poll overview
         <ParticipantRowView
           key={i}
-          color={participant.color}
+          color={participant.color}  // each participant icon gets their own color
           sidebarWidth={sidebarWidth}
           columnWidth={columnWidth}
           participantId={`participant${i}`}
           name={participant.name}
           votes={options.map((_, i) => {
-            return participant.votes.some((vote) => vote === i) ? "yes" : "no";
+            return participant.votes.some((vote) => vote === i) ? "yes" : "no"; // see which participants has voted for which time. 
           })}
         />
       ))}
